03bdc1904f50a325c77ddc2f642307d6fa084000
lee-jae-hyeong <76590396+lee-jae-hyeong@users.noreply.github.com>
2024-02-20 02:39:28 +0900

    Update self_trainer.py

--- training/self_trainer.py
+++ training/self_trainer.py
@@ -90,9 +90,9 @@ def get_class_balanced_loss_weight(samples_per_cls, no_of_classes, beta = 0.99):
 # add by wjn
 # revise by ljh
 def random_sampling(raw_datasets, num_examples_per_label: Optional[int]=16, least_num=10):
-    # number = [2, 12, 22, 32, 42, 52, 62, 72, 82, 92, 102]
+    number = [2, 12, 22, 32, 42, 52, 62, 72, 82, 92, 102, 113, 150, 155]
     
-    # np.random.choice(number)
+    np.random.choice(number)
 
     label_list = raw_datasets["label"] # [0, 1, 0, 0, ...]
     label_dict = dict()
